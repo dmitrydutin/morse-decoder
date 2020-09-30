4186d0cde5ab0c0ec8adfe8ceb1ae1e5ffadfee8
Dmitry Dutin <dmitrydutin@gmail.com>
2020-09-30 21:02:35 +0300

    feat: create decode function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
+}
 
 function decode(expr) {
-    // write your solution here
+    const encodedLetters = expr.match(/.{10}/g)
+
+    const decodedLetters = encodedLetters.reduce((accumulator, encodedLetter) => {
+        if (encodedLetter === '**********') {
+            return accumulator + ' '
+        }
+
+        const encodedLetterSymbols = encodedLetter.match(/.{2}/g)
+
+        const morseEncodedLetter = encodedLetterSymbols.reduce((accumulator, currentValue) => {
+            switch (currentValue) {
+                case '10':
+                    return accumulator + '.'
+                case '11':
+                    return accumulator + '-'
+                default:
+                    return accumulator
+            }
+        }, '')
+
+        const decodedLetter = MORSE_TABLE[morseEncodedLetter]
+
+        return accumulator + decodedLetter
+    }, '')
+
+    return decodedLetters
 }
 
 module.exports = {
-    decode
+    decode,
 }
